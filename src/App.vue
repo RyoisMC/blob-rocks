89c3045125ed658095ae71417c8f4c43c74bd197
Ryois <ryois@ryois.me>
2020-11-22 16:13:15 -0500

    Initial Repo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <particles/>
+  </div>
+</template>
+
+<script>
+import particles from "./components/particles.vue";
+
+export default {
+  name: "App",
+  components: {
+    particles
+  }
+};
+</script>
+
+<style>
+body, html {
+    margin:0;
+    height:100%;
+    overflow:hidden;
+}
+#app {
+    background-image:url("https://pcmr.rocks/blobbanner.jpg");
+    height:100%;
+    background-position:center;
+    background-repeat:no-repeat;
+    background-size:cover
+}
+</style>
